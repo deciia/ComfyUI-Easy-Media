06b27d879b9185ba3def08466e9944951f8470f7
deciia <deciia@users.noreply.github.com>
2026-09-23 07:51:52 +0800

    build: rebuild frontend dist after upstream merge

--- dist/release/chunks/timeline-utils-pdmq3khc.js
+++
@@ -1,1 +0,0 @@
-import{B,xe,Ze,ye,Qe}from"./index-debrw42j.js";export{B as computeSlotItems,Qe as createDefaultTimelineData,ye as framesToSeconds,xe as scaleImageItemsToDuration,Ze as secondsToFrames};
---
+++ dist/release/chunks/timeline-utils-t8vwfk3k.js
@@ -0,0 +1,1 @@
+import{B,ve,Je,be,et}from"./index-h14r7y9p.js";export{B as computeSlotItems,et as createDefaultTimelineData,be as framesToSeconds,ve as scaleImageItemsToDuration,Je as secondsToFrames};
